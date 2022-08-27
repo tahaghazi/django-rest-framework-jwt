a8761fb99de1fbb8789dee37140f4edf3d8646fc
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2022-08-27 23:38:23 +0200

    Allow Compatilibity With PyJWT>2.0.0

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ def get_version(package):
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
 
 
-name = 'djangorestframework-jwt'
+name = 'djangorestframework-jwt-4'
 version = get_version('rest_framework_jwt')
 package = 'rest_framework_jwt'
 description = 'JSON Web Token based authentication for Django REST framework'
-url = 'https://github.com/jpadilla/django-rest-framework-jwt'
+url = 'https://github.com/tahaghazy/django-rest-framework-jwt'
 author = 'Jose Padilla'
 author_email = 'jpadilla@getblimp.com'
 license = 'MIT'
